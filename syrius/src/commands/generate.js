79e1d76ccd4d5b2e27bd82527b8ae8c22d3978b5
Intern-Yago <yagovictorbotafogo@gmail.com>
2023-01-12 11:19:05 -0300

    ajustando erros

---
+++ syrius/src/commands/generate.js
@@ -0,0 +1,21 @@
+module.exports = {
+  name: 'generate',
+  alias: ['g'],
+  run: async (toolbox) => {
+    const {
+      parameters,
+      template,
+      print: { info },
+    } = toolbox
+
+    const name = parameters.first
+
+    await template.generate({
+      template: 'model.js.ejs',
+      target: `models/${name}-model.js`,
+      props: { name },
+    })
+
+    info(`Generated file at models/${name}-model.js`)
+  },
+}
