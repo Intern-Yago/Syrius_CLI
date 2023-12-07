0bf25b1c78a187029eaf2583e865c7798a105949
Intern-Yago <yagovictorbotafogo@gmail.com>
2023-12-07 14:16:20 -0300

    add images

--- syrius/src/extensions/createFilesPWA.js
+++ syrius/src/extensions/createFilesPWA.js
@@ -1,5 +1,5 @@
 module.exports = (toolbox)=>{
-    const {template, patching, print:{success, error, muted, highlight}} = toolbox
+    const {template, patching,filesystem, print:{success, error, muted, highlight}} = toolbox
     async function createFilesPWA(name,path){
         let sucessos = []
         async function generateSWInit(path){
@@ -35,27 +35,49 @@ module.exports = (toolbox)=>{
         await generateManifest(name, path)
 
         async function editIndex(){
-            patching.patch('index.html', {insert: '<link rel="manifest" href="manifest.json">', after: '<head>'})
+            patching.patch('index.html', {insert: '<link rel="manifest" href="manifest.json"> <script defer src="./initSW.js"></script>', after: '<head>'})
             muted("Index html mudado com sucesso")
             sucessos.push('index')
         }
         await editIndex()
 
+        async function generateImages(path){
+            await template.generate({
+                template: 'pwa/screenshot1.svg.ejs',
+                target: `assets/screenshot1.svg`,
+            })
+            await template.generate({
+                template: 'pwa/screenshot2.svg.ejs',
+                target: `assets/screenshot2.svg`,
+            })
+            await template.generate({
+                template: 'pwa/favicon.svg.ejs',
+                target: `assets/favicon.svg`,
+            })
+            
+            muted("Imagens geradas")
+            sucessos.push("imagens")
+        }
+        await generateImages(path)
+
         if(sucessos.length >=3){
             success("PWA gerado com sucesso")
             highlight("Faça alterações necessárias no manifest")
         }else{
-            if(!('manifest' in sucesso)){
+            if(!('manifest' in sucessos)){
                 error('Falha na criação do manifest')
             }
-            else if(!('SW' in sucesso)){
+            else if(!('SW' in sucessos)){
                 error('Falha na criação do sw.js')
             }
-            else if(!('Init' in sucesso)){
+            else if(!('Init' in sucessos)){
                 error('Falha na criação do SW init')
             }
-            else if(!('index' in sucesso)){
+            else if(!('index' in sucessos)){
                 error('Falha na edição do index.html')
+            }
+            else if(!('imagens' in sucessos)){
+                error('Falha ao gerar as imagens')
             }
             else{
                 error("Erro desconhecido")
